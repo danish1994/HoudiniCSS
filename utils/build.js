465de5a0208ee5d66c969573cef95acdec1b50ef
Danish Kamal <danish5095@gmail.com>
2021-07-17 00:02:48 +0530

    Added Build Process

---
+++ utils/build.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const hbs = require('handlebars');
+
+const projectBasePath = path.join(__dirname, '../');
+const basePath = path.join(projectBasePath, './examples');
+const indexPath = path.join(projectBasePath, './index.html');
+const templatePath = path.join(projectBasePath, './utils/index.hbs');
+
+function getAllExamples(currentPath) {
+    const examples = [];
+
+    function convertToTitleCase(str) {
+        const result = str.replace(/([A-Z])/g, " $1");
+        return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
+    function scanPaths(currentPath) {
+        const files = fs.readdirSync(currentPath);
+        if (files.includes('index.html')) {
+            examples.push({
+                path: currentPath.replace(projectBasePath, './'),
+                name: convertToTitleCase(currentPath.split('/').pop())
+            })
+        } else {
+            for (const file of files) {
+                scanPaths(path.join(currentPath, file));
+            }
+        }
+    }
+
+    scanPaths(currentPath);
+    return examples;
+}
+
+function writeToTemplate(examples, templatePath) {
+    const template = fs.readFileSync(templatePath, 'utf8');
+    const compiled = hbs.compile(template);
+    const html = compiled({examples});
+    console.log(html);
+    fs.writeFileSync(indexPath, html);
+}
+
+
+const examples = getAllExamples(basePath);
+writeToTemplate(examples, templatePath);
+
+
